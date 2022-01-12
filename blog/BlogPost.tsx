757ed87f59d9a02fc706c055c69811c3cf862686
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2022-01-12 15:35:47 -0600

    change apostephres to "&apos;" - this fixes an ESLint error, not sure what value it provides though

--- blog/BlogPost.tsx
+++ blog/BlogPost.tsx
@@ -8,9 +8,9 @@ export default function () {
         <p>
           This is the first entry in my blog. Depending on your definition, this
           might be my first entry in <em>any</em> blog. I absolutely love
-          creating text-driven content, yet I've shy'd away from publishing
-          compositions publicly. Here is to changing that narrative. Welcome to
-          my first publication.
+          creating text-driven content, yet I&apos;ve shy&apos;d away from
+          publishing compositions publicly. Here is to changing that narrative.
+          Welcome to my first publication.
         </p>
         <p>Hi, my name is Chance. I li</p>
         <p></p>
